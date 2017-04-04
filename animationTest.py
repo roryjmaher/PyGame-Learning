bb13efbf96018337358da9511572d6e35a33d52c
Brian O'Neill <brianon.run@Gmail.com>
2017-04-04 12:12:04 +0100

    Added some color Constants
    #               R    G    B
    WHITE       = (255, 255, 255)
    GRAY        = (185, 185, 185)
    BLACK       = (  0,   0,   0)
    RED         = (155,   0,   0)
    LIGHTRED    = (175,  20,  20)
    GREEN       = (  0, 155,   0)
    LIGHTGREEN  = ( 20, 175,  20)
    BLUE        = (  0,   0, 155)
    LIGHTBLUE   = ( 20,  20, 175)
    YELLOW      = (155, 155,   0)
    LIGHTYELLOW = (175, 175,  20)

--- animationTest.py
+++ animationTest.py
@@ -18,6 +18,19 @@ import pygame
 from Player import Player
 from Input import Input
 
+#               R    G    B
+WHITE       = (255, 255, 255)
+GRAY        = (185, 185, 185)
+BLACK       = (  0,   0,   0)
+RED         = (155,   0,   0)
+LIGHTRED    = (175,  20,  20)
+GREEN       = (  0, 155,   0)
+LIGHTGREEN  = ( 20, 175,  20)
+BLUE        = (  0,   0, 155)
+LIGHTBLUE   = ( 20,  20, 175)
+YELLOW      = (155, 155,   0)
+LIGHTYELLOW = (175, 175,  20)
+
 
 def load_images_from_directory(path):
     scaled_images = []
