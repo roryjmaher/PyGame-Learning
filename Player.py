f30899f32d3ca32c75732935d0dfb6552db4b5cb
Brian O'Neill <brianon.run@Gmail.com>
2017-04-04 09:45:11 +0100

    Added sliding

--- Player.py
+++ Player.py
@@ -17,9 +17,11 @@ class Player(object):
         self.running_images = running_images
         self.walking_images = walking_images
         self.standing_images = standing_images
+        self.sliding_images = sliding_images
         self.current_running_image = 0
         self.current_walking_image = 0
         self.current_standing_image = 0
+        self.current_sliding_image = 0
         self.is_winner = False
         self.x_coordinate = x_coordinate
         self.last_input = Input.NONE
@@ -28,6 +30,21 @@ class Player(object):
         self.current_standing_image += 1
         if self.current_standing_image >= len(self.standing_images):
             self.current_standing_image = 0
+            
+    def next_walking_image(self):
+        self.current_walking_image += 1
+        if self.current_walking_image >= len(self.walking_images):
+            self.current_walking_image = 0
+
+    def next_running_image(self):
+        self.current_running_image += 1
+        if self.current_running_image >= len(self.running_images):
+            self.current_running_image = 0
+            
+    def next_sliding_image(self):
+        self.current_sliding_image += 1
+        if self.current_sliding_image >= len(self.sliding_images):
+            self.current_sliding_image = 0
 
     " gets the next x position of the player based on the current x_coordinate and the velocity "
     def get_new_position(self):
